fbf17c8de5e5526eda1fbabce162f1c594707f9b
Jason Williams <jasonwilliams2308@gmail.com>
2018-02-09 19:43:36 +0000

    navigation links

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => (
-  <nav className="navbar navbar-dark bg-primary">
+  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
     <div className="container">
-      <span className="navbar-brand mb-0 h1">Elder Quiz</span>
+      <Link className="navbar-brand" to="/">
+        Navbar
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <Link className="nav-item nav-link " to="/">
+            Home <span className="sr-only">(current)</span>
+          </Link>
+          <Link className="nav-item nav-link" to="/api">
+            Api
+          </Link>
+        </div>
+      </div>
     </div>
   </nav>
 );
