072f0bc6dd9a3404a337f3f2eb176bc2157c1ec4
Jason Williams <jasonwilliams2308@gmail.com>
2018-02-15 08:51:23 +0000

    .

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/static", express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use("/", index);
 
