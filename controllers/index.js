abbbc6d7532f5241ea3b3dfb62143eedf02f173a
Jason Williams <jasonwilliams2308@gmail.com>
2018-02-15 08:41:32 +0000

    modifications to heroku files

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res) => {
-  res.render("index", { title: "Express", project: "Elder Project" });
+  res.render("index");
 });
 
 module.exports = router;
